8ab73f535e28e749a30e0a0eec5bdb53b39a0dc3
Jeremy Englert <jeremy@theadgarage.com>
2013-12-08 14:13:51 -0700

    Removed byline from custom-page.

--- page-custom.php
+++ page-custom.php
@@ -19,10 +19,6 @@ Template Name: Custom Page Example
 						    <header class="article-header">
 							
 							    <h1 class="page-title"><?php the_title(); ?></h1>
-                  <p class="byline vcard"><?php
-                    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'jointstheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'jointstheme')), joints_get_the_author_posts_link());
-                  ?></p>
-
 						
 						    </header> <!-- end article header -->
 					
