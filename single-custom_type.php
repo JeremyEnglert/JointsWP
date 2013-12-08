7669bbb0663e432d5d5a44af7f511b134c4a5299
Jeremy Englert <jeremy@theadgarage.com>
2013-12-08 14:23:16 -0700

    Removed Byline

--- single-custom_type.php
+++ single-custom_type.php
@@ -28,9 +28,6 @@ single-bookmarks.php
 						    <header class="article-header">
 							
 							    <h1 class="single-title custom-post-type-title"><?php the_title(); ?></h1>
-                  <p class="byline vcard"><?php
-                    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'jointstheme'), get_the_time('Y-m-j'), get_the_time(__('F js, Y', 'jointstheme')), joints_get_the_author_posts_link(), get_the_term_list( get_the_ID(), 'custom_cat', "" ));
-                  ?></p>
 						
 						    </header> <!-- end article header -->
 					
