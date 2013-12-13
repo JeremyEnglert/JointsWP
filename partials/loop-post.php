576111a0c49d6229b2defd36f8066b7aaff16aaa
Jeremy Englert <jeremy@theadgarage.com>
2013-12-12 20:44:37 -0700

    removed link_pages function

--- partials/loop-post.php
+++ partials/loop-post.php
@@ -8,7 +8,6 @@
 					
     <section class="entry-content clearfix" itemprop="articleBody">
 		<?php the_content(); ?>
-		<?php wp_link_pages(); ?>
 	</section> <!-- end article section -->
 						
 	<footer class="article-footer">
