f7aa8198c1be7adb6879c916fd566b7215fd9960
Jeremy Englert <jeremy@theadgarage.com>
2013-12-14 22:33:43 -0700

    Added Foundation Icons 3 functionality.

--- header.php
+++ header.php
@@ -1,5 +1,10 @@
 <!doctype html>
 
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
 	<head>
 		<meta charset="utf-8">
 
--- library/joints.php
+++ library/joints.php
@@ -129,6 +129,9 @@ function joints_scripts_and_styles() {
    
     // register main stylesheet
     wp_register_style( 'joints-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
+    
+    // register foundation icons
+    wp_register_style( 'foundation-icons', get_stylesheet_directory_uri() . '/library/css/icons/foundation-icons.css', array(), '', 'all' );
 
     // comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
@@ -142,6 +145,7 @@ function joints_scripts_and_styles() {
     wp_enqueue_script( 'joints-modernizr' );
     wp_enqueue_script ('foundation-js');
     wp_enqueue_style( 'joints-stylesheet' );
+     wp_enqueue_style( 'foundation-icons' );
 
     $wp_styles->add_data( 'joints-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
 
