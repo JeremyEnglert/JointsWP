a0cffaee4738b713a24d0fda80f98b0bccab618d
Jeremy Englert <jeremy@theadgarage.com>
2014-01-10 21:46:49 -0700

    Edited error causing menu to not close in IE

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!doctype html>
 
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 
 	<head>
 		<meta charset="utf-8">
@@ -52,7 +52,7 @@
 						
 						<?php get_template_part( 'partials/nav', 'topbar' ); ?>
 						
-						<?php get_template_part( 'partials/nav', 'offcanvas' ); ?>
+						<?php get_template_part( 'partials/nav', 'offcanvas-sidebar' ); ?>
 						
 					</div> <!-- end #inner-header -->
 	
