8d3997a45113c396dbd1d441bc58587ec18a05aa
Jeremy Englert <jeremy@theadgarage.com>
2014-02-09 20:43:21 -0700

    Added loop template part.

--- archive-custom_type.php
+++ archive-custom_type.php
@@ -3,61 +3,39 @@
 			<div id="content">
 			
 				<div id="inner-content" class="row clearfix">
-				
-				    <div id="main" class="large-8 medium-8 columns first clearfix" role="main">
-				
-						<h1 class="archive-title h2"><?php post_type_archive_title(); ?></h1>
+			
+				    <div id="main" class="large-8 medium-8 columns clearfix" role="main">
 
 					    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-					    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
-						    <header class="article-header">
-							
-							    <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-                  <p class="byline vcard"><?php
-                    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'jointstheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'jointstheme')), joints_get_the_author_posts_link());
-                  ?></p>
-						
-						    </header> <!-- end article header -->
-					
-						    <section class="entry-content clearfix">
-						
-							    <?php the_excerpt(); ?>
-
-						    </section> <!-- end article section -->
-						
-						    <footer class="article-footer">
-							
-						    </footer> <!-- end article footer -->
-					
-					    </article> <!-- end article -->
+					    	<?php get_template_part( 'partials/loop', 'archive' ); ?>
 					
 					    <?php endwhile; ?>	
 					
 					        <?php if (function_exists('joints_page_navi')) { ?>
 					            <?php joints_page_navi(); ?>
 					        <?php } else { ?>
+					        
 					            <nav class="wp-prev-next">
 					                <ul class="clearfix">
 					        	        <li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "jointstheme")) ?></li>
 					        	        <li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "jointstheme")) ?></li>
 					                </ul>
 					            </nav>
-					        <?php } ?>
+					        <?php } ?>		
 					
 					    <?php else : ?>
-					
-                <?php get_template_part( 'partials/missing', 'content' ); ?>
+					    
+    						<?php get_template_part( 'partials/content', 'missing' ); ?>
 					
 					    <?php endif; ?>
 			
-    				</div> <!-- end #main -->
+				    </div> <!-- end #main -->
     
-	    			<?php get_sidebar(); ?>
-                
-                </div> <!-- end #inner-content -->
-                
+				    <?php get_sidebar(); ?>
+				    
+				</div> <!-- end #inner-content -->
+    
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
