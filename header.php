6c4ae299374bce7f619b1bebac8525dcc51e450e
Danny Weeks <danny@dannyweeks.com>
2014-02-12 12:02:26 +0000

    Removed rogue </> tag and did a little tidy up

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
   	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<?php wp_head(); ?>
-		
+
 		<!-- Drop Google Analytics here -->
 		<!-- end analytics -->
 
@@ -41,24 +41,30 @@
 	<div class="off-canvas-wrap">
 		<div class="inner-wrap">
 			<div id="container">
-	
+
 				<header class="header" role="banner">
-	
+
 					<div id="inner-header" class="row">
 						<div class="large-12 columns">
-							<h1><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></>
-							<small><?php  bloginfo('description'); ?></small></h1>
-						</div>					
-						
-						 <?php  get_template_part( 'partials/nav', 'offcanvas' ); ?> 
-						
+							<h1>
+								<a href="<?php echo home_url(); ?>" rel="nofollow">
+									<?php bloginfo('name'); ?>
+								</a>
+								<small>
+									<?php  bloginfo('description'); ?>
+								</small>
+							</h1>
+						</div>
+
+						 <?php  get_template_part( 'partials/nav', 'offcanvas' ); ?>
+
 						 <?php // get_template_part( 'partials/nav', 'topbar' ); ?>
-						 
-						 <?php // get_template_part( 'partials/nav', 'offcanvas-sidebar' ); ?>  								
-						
+
+						 <?php // get_template_part( 'partials/nav', 'offcanvas-sidebar' ); ?>
+
 						<!-- You only need to use one of the above navigations.
 							 Offcanvas-sidebar adds a sidebar to a "right" offcanavas menus. -->
-										
+
 					</div> <!-- end #inner-header -->
-	
+
 				</header> <!-- end header -->
