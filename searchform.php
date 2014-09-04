c28c18d2616b7bea83d564adbe7888e6ace0958c
Michael Scharf <mrscharf@gmail.com>
2014-09-04 14:07:21 -0400

    Cleanup commented code, change search form to be more HTML5 compliant

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>" >
 	<label class="screen-reader-text" for="s"><?php echo __('Search for:', 'jointstheme'); ?></label>
-	<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php echo esc_attr__('Search the Site...','jointstheme') ?>" />
+	<input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php echo esc_attr__('Search the Site...','jointstheme') ?>" />
 	<input type="submit" id="searchsubmit" class="button" value="<?php echo esc_attr__('Search'); ?>" />
 </form>
