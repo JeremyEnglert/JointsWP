785bbfaf4d5b917c50378154901a4a78db3b48de
Jeremy Englert <Jeremy@theadgarage.com>
2015-01-16 18:09:06 -0700

    Changed default archive style.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 				    <div id="main" class="large-8 medium-8 columns" role="main">
 					 
 					  <!-- To see additional archive styles, visit the /partials directory -->
-					    <?php get_template_part( 'partials/loop', 'archive-grid' ); ?>
+					    <?php get_template_part( 'partials/loop', 'archive' ); ?>
 								
 				    </div> <!-- end #main -->
     
