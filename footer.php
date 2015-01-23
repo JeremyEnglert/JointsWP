a5aad51e477b96c53c3b725947055f069d302c51
Allen <crepe@jarday.com>
2015-01-23 09:47:33 -0600

    Cleanup footer
    Realign indented tags and close <html>

--- footer.php
+++ footer.php
@@ -2,17 +2,18 @@
 						<div id="inner-footer" class="row">
 							<div class="large-12 medium-12 columns">
 								<nav role="navigation">
-		    						<?php joints_footer_links(); ?>
-		    					</nav>
-		    				</div>
-			                <div class="large-12 medium-12 columns">		
+		    					<?php joints_footer_links(); ?>
+		    				</nav>
+		    			</div>
+			        <div class="large-12 medium-12 columns">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-							</div>		
-						</div> <!-- end #inner-footer -->			
+							</div>
+						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
 				</div> <!-- end #container -->
 			</div> <!-- end .inner-wrap -->
-		</div> <!-- end .off-canvas-wrap -->		
+		</div> <!-- end .off-canvas-wrap -->
 		<!-- all js scripts are loaded in library/joints.php -->
 		<?php wp_footer(); ?>
 	</body>
+</html> <!-- end page -->
