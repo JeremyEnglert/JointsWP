f38e7b2bfba7164f8fda8cbcc4cb23c9b3121d50
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2015-02-15 22:17:57 -0700

    Clean up

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,7 +1,6 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	
-	<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
-							
+	<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
 		<header class="article-header">
 			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 			</h2>
@@ -15,8 +14,7 @@
 							
 		<footer class="article-footer">
 	    	<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
-		</footer> <!-- end article footer -->
-							    						
+		</footer> <!-- end article footer -->				    						
 	</article> <!-- end article -->
 
 <?php endwhile; ?>	
