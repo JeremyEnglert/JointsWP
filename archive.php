b401aa4d55f6e4c94083769706e74fa665327105
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2015-02-16 22:20:18 -0700

    Clean up

--- archive.php
+++ archive.php
@@ -7,10 +7,8 @@
 				    <div id="main" class="large-8 medium-8 columns first" role="main">
 					    
 				    	<header>
-				    		<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-							?>
+				    		<h1 class="page-title"><?php the_archive_title();?></h1>
+							<?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
 				    	</header>
 				
 				    	<!-- To see additional archive styles, visit the /parts directory -->
