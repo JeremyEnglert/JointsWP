664c838de4c4d9c76befb0be20a8a63c55343d1d
Gifford Nowland <gifford.nowland@westguardsolutions.com>
2015-05-01 22:14:47 -0500

    div tag not closed correctly
    missing forwardslash

--- partials/loop-archive-accordion.php
+++ partials/loop-archive-accordion.php
@@ -5,7 +5,7 @@
 		<a href="#panel<?php the_ID(); ?>" title="<?php the_title_attribute(); ?>"> <h4 class="fi-plus "> <?php the_title(); ?></h4></a>
 	    <div class="content" id="panel<?php the_ID(); ?>">
 			<?php the_content(); ?>
-	    <div> <!-- end article section -->
+	    </div> <!-- end article section -->
 	</dd>		
 <?php endwhile; ?>
 </dl>	
