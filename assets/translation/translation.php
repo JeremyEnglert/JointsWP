131bee9e2a75e871e42984ac6e02f347fa23f30d
Mikkel Breum <mail@mikkelbreum.com>
2015-05-06 23:04:41 +0200

    added proper hook
    wrapped load_theme_textdomain in function and hooked to after_setup_theme

--- assets/translation/translation.php
+++ assets/translation/translation.php
@@ -2,20 +2,18 @@
 /* Welcome to joints :)
 Thanks to the fantastic work by joints users, we've now
 the ability to translate joints into different languages.
-
 */
 
 
-
 // Adding Translation Option
-load_theme_textdomain( 'jointstheme', get_template_directory() .'/assets/translation' );
+add_action('after_setup_theme', 'load_translations');
+function load_translations(){
+	load_theme_textdomain( 'jointstheme', get_template_directory() .'/assets/translation' );
+	
 	$locale = get_locale();
 	$locale_file = get_template_directory() ."/assets/translation/$locale.php";
-if ( is_readable($locale_file) ) require_once($locale_file);
-
-
-
-
+	if ( is_readable($locale_file) ) require_once($locale_file);
+}
 
 
 
