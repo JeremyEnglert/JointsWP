88bb2293fad71aa75a4e1f8bd869292f7037c48a
Josh Winn <jw@joshuawinn.com>
2015-07-08 15:58:32 -0400

    Fixing repeated UL with class off-canvas-list (repeated UL generated by joints_off_canvas() should not be nested within another. Screenshot: http://d.pr/i/16PQB )

--- parts/nav-top-offcanvas.php
+++ parts/nav-top-offcanvas.php
@@ -25,8 +25,7 @@
 						
 <aside class="left-off-canvas-menu show-for-small-only">
 	<ul class="off-canvas-list">
-		<li><label>Navigation</label></li>
-			<?php joints_off_canvas(); ?>    
+		<?php joints_off_canvas(); ?>    
 	</ul>
 </aside>
 			
