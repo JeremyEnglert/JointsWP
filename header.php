f4056095ec6dacfdcdcd0292179190295828087e
Paul Vincent Beigang <pbeigang@gmail.com>
2015-08-28 10:37:17 +0200

    Improve indentation

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 		<![endif]-->
 		<meta name="msapplication-TileColor" content="#f01d4f">
 		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
-	    <meta name="theme-color" content="#121212">
+	    	<meta name="theme-color" content="#121212">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
