11370b15d5cbf486e98adaa0e13dfc7b7bb14b82
Jeremy Englert <jeremy@cmsadgroup.com>
2015-08-31 00:55:54 -0700

    Cleaned up script enqueueing

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -1,5 +1,5 @@
 <?php
-function joints_scripts_and_styles() {
+function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
     $theme_version = wp_get_theme()->Version;
 
@@ -7,16 +7,16 @@ function joints_scripts_and_styles() {
 	wp_deregister_script('jquery');
 	
 	// Loads jQuery from vendor
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/vendor/foundation/js/vendor/jquery.js', array(), '2.1.3', true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/vendor/foundation/js/vendor/jquery.js', array(), '', true );
     
     // Modernizr from vendor
-    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/vendor/foundation/js/vendor/modernizr.js', array(), '2.8.3', true );
+    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/vendor/foundation/js/vendor/modernizr.js', array(), '', true );
     
     // Adding Foundation scripts file in the footer
-   wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/min/foundation.min.js', array( 'jquery' ), '', true );
+   wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.min.js', array( 'jquery' ), '', true );
     
     // Adding scripts file in the footer
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/min/scripts.js', array( 'jquery' ), '', true );
+    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', true );
    
     // Register main stylesheet
     wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
@@ -29,5 +29,5 @@ function joints_scripts_and_styles() {
       wp_enqueue_script( 'comment-reply' );
     }
 }
-add_action('wp_enqueue_scripts', 'joints_scripts_and_styles', 999);
+add_action('wp_enqueue_scripts', 'site_scripts', 999);
 ?>
