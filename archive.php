8b86f3c8128876cfe3ce0c44ab31e4839c759fd1
Jeremy Englert <jeremy@cmsadgroup.com>
2015-09-05 02:26:00 -0700

    Added MAIN HTML5 element

--- archive.php
+++ archive.php
@@ -1,25 +1,25 @@
 <?php get_header(); ?>
 			
-			<div id="content">
-			
-				<div id="inner-content" class="row">
-				
-				    <div id="main" class="large-8 medium-8 columns first" role="main">
-					    
-				    	<header>
-				    		<h1 class="page-title"><?php the_archive_title();?></h1>
-							<?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
-				    	</header>
-				
-				    	<!-- To see additional archive styles, visit the /parts directory -->
-				    	<?php get_template_part( 'parts/loop', 'archive' ); ?>					
-			
-    				</div> <!-- end #main -->
-    
-	    			<?php get_sidebar(); ?>
-                
-                </div> <!-- end #inner-content -->
-                
-			</div> <!-- end #content -->
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+		
+		    <main id="main" class="large-8 medium-8 columns first" role="main">
+			    
+		    	<header>
+		    		<h1 class="page-title"><?php the_archive_title();?></h1>
+					<?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
+		    	</header>
+		
+		    	<!-- To see additional archive styles, visit the /parts directory -->
+		    	<?php get_template_part( 'parts/loop', 'archive' ); ?>					
+	
+			</main> <!-- end #main -->
+	
+			<?php get_sidebar(); ?>
+	    
+	    </div> <!-- end #inner-content -->
+	    
+	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
