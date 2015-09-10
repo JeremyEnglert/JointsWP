b9a7c8cf47303c31e7cced14b52b01088cb0c48e
Jeremy Englert <jeremy@cmsadgroup.com>
2015-09-09 23:12:48 -0700

    Better indentation

--- page.php
+++ page.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
+	
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-8 medium-8 columns" role="main">
 			
-			<div id="content">
-			
-				<div id="inner-content" class="row">
-			
-				    <main id="main" class="large-8 medium-8 columns" role="main">
-					
-					    	<?php get_template_part( 'parts/loop', 'page' ); ?>
-					    					
-    				</main> <!-- end #main -->
-    
-				    <?php get_sidebar(); ?>
-				    
-				</div> <!-- end #inner-content -->
-    
-			</div> <!-- end #content -->
+			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+			    					
+			</main> <!-- end #main -->
+
+		    <?php get_sidebar(); ?>
+		    
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
--- template-full-width.php
+++ template-full-width.php
@@ -6,18 +6,18 @@ Template Name: Full Width (No Sidebar)
 
 <?php get_header(); ?>
 			
-			<div id="content">
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-12 medium-12 columns" role="main">
 			
-				<div id="inner-content" class="row">
-			
-				    <main id="main" class="large-12 medium-12 columns" role="main">
-					
-						<?php get_template_part( 'parts/loop', 'page' ); ?>
-					    					
-    				</main> <!-- end #main -->
-				    
-				</div> <!-- end #inner-content -->
-    
-			</div> <!-- end #content -->
+				<?php get_template_part( 'parts/loop', 'page' ); ?>
+			    					
+			</main> <!-- end #main -->
+		    
+		</div> <!-- end #inner-content -->
+	
+	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
