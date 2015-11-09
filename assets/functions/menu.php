711be7eec1aff2459a96d66e564d99ded7089986
Jeremy Englert <jeremy@cmsadgroup.com>
2015-11-09 02:07:30 -0700

    Foundation 6 Menu types

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -11,12 +11,11 @@ register_nav_menus(
 function joints_top_nav() {
 	 wp_nav_menu(array(
         'container' => false,                           // Remove nav container
-        'menu_class' => 'vertical medium-horizontal menu',       // Adding custom nav class
-        'items_wrap' => '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
+        'menu_class' => 'dropdown menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-dropdown-menu>%3$s</ul>',
         'theme_location' => 'main-nav',        			// Where it's located in the theme
         'depth' => 5,                                   // Limit the depth of the nav
         'fallback_cb' => false,                         // Fallback function (see below)
-       
         'walker' => new Foundation_Menu_Walker()
     ));
 } /* End Top Menu */
@@ -24,17 +23,12 @@ function joints_top_nav() {
 // The Footer Menu
 function joints_footer_links() {
     wp_nav_menu(array(
-    	'container' => '',                              // Remove nav container
-    	'container_class' => 'footer-links clearfix',   // Class of container (should you choose to use it)
-    	'menu' => __( 'Footer Links', 'jointswp' ),   // Nav name
-    	'menu_class' => 'sub-nav',      // Adding custom nav class
+    	'container' => 'false',                              // Remove nav container
+    	'menu' => __( 'Footer Links', 'jointswp' ),   	// Nav name
+    	'menu_class' => 'menu',      					// Adding custom nav class
     	'theme_location' => 'footer-links',             // Where it's located in the theme
-    	'before' => '',                                 // Before the menu
-        'after' => '',                                  // After the menu
-        'link_before' => '',                            // Before each link
-        'link_after' => '',                             // After each link
         'depth' => 0,                                   // Limit the depth of the nav
-    	'fallback_cb' => 'joints_footer_links_fallback'  // Fallback function
+    	'fallback_cb' => ''  							// Fallback function
 	));
 } /* End Footer Menu */
 
