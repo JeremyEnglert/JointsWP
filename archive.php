80e0f33448c223ea01cb3bd06bbcdd6b9b0d8bad
Jeremy Englert <jeremy@theadgarage.com>
2015-11-13 16:03:39 -0700

    Removed stray class

--- archive.php
+++ archive.php
@@ -4,7 +4,7 @@
 	
 		<div id="inner-content" class="row">
 		
-		    <main id="main" class="large-8 medium-8 columns first" role="main">
+		    <main id="main" class="large-8 medium-8 columns" role="main">
 			    
 		    	<header>
 		    		<h1 class="page-title"><?php the_archive_title();?></h1>
