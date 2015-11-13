bc902a2ff544cbe39a55b1cbcfae7f5ef0337dce
Jeremy Englert <jeremy@cmsadgroup.com>
2015-11-12 20:19:49 -0700

    Giving credit where credit is due (@brettsmason)

--- assets/functions/menu-walkers.php
+++ assets/functions/menu-walkers.php
@@ -1,4 +1,5 @@
 <?php 
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
 class Foundation_Menu_Walker extends Walker_Nav_Menu {
     function start_lvl(&$output, $depth = 0, $args = Array() ) {
         $indent = str_repeat("\t", $depth);
