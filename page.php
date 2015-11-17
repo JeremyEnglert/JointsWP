d4765cf22ee4bb3817953799eb4e5d3b392a73c1
Jeremy Englert <jeremy@cmsadgroup.com>
2015-11-16 19:39:25 -0700

    Fixed loop

--- page.php
+++ page.php
@@ -5,8 +5,12 @@
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-8 medium-8 columns" role="main">
-			
+				
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+			    
+			    <?php endwhile; endif; ?>							
 			    					
 			</main> <!-- end #main -->
 
--- template-full-width.php
+++ template-full-width.php
@@ -11,8 +11,12 @@ Template Name: Full Width (No Sidebar)
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-12 medium-12 columns" role="main">
-			
-				<?php get_template_part( 'parts/loop', 'page' ); ?>
+				
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<?php get_template_part( 'parts/loop', 'page' ); ?>
+					
+				<?php endwhile; endif; ?>							
 
 			</main> <!-- end #main -->
 		    
