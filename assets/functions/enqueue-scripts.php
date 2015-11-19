2f558f608f30b3419e62e05b73b3e5a3e96ad861
Jeremy Englert <jeremy@cmsadgroup.com>
2015-11-18 21:33:57 -0700

    Enqueue minified styles

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -15,7 +15,7 @@ function site_scripts() {
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'vendor' ), '', true );
    
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.min.css', array(), '', 'all' );
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
