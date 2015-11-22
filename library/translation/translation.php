c5b1406bdb2d3420bb412ded740300d0e2e01521
Piet Bos <info@senlinonline.com>
2015-11-22 10:30:41 +0800

    change textdomain to name of theme slug

--- library/translation/translation.php
+++ library/translation/translation.php
@@ -8,7 +8,7 @@ the ability to translate joints into different languages.
 
 
 // Adding Translation Option
-load_theme_textdomain( 'jointstheme', get_template_directory() .'/library/translation' );
+load_theme_textdomain( 'jointswp', get_template_directory() .'/library/translation' );
 	$locale = get_locale();
 	$locale_file = get_template_directory() ."/library/translation/$locale.php";
 if ( is_readable($locale_file) ) require_once($locale_file);
