8c16731162238dcc0e244bbb53fd72fa55fdc61d
Piet Bos <info@senlinonline.com>
2015-11-22 10:05:35 +0800

    i18n strings

--- comments.php
+++ comments.php
@@ -5,11 +5,11 @@ The comments page for joints
 
 // Do not delete these lines
   if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-    die ('Please do not load this page directly. Thanks!');
+    die ( __( 'Please do not load this page directly. Thanks!', 'jointswp' ) );
 
   if ( post_password_required() ) { ?>
   	<div class="alert help">
-    	<p class="nocomments"><?php _e("This post is password protected. Enter the password to view comments.", "jointstheme"); ?></p>
+    	<p class="nocomments"><?php _e( 'This post is password protected. Enter the password to view comments.', 'jointswp' ); ?></p>
   	</div>
   <?php
     return;
@@ -19,8 +19,7 @@ The comments page for joints
 <!-- You can start editing here. -->
 
 <?php if ( have_comments() ) : ?>
-	<h3 id="comments" class="h2"><?php comments_number(__('<span>No</span> Responses', 'jointstheme'), __('<span>One</span> Response', 'jointstheme'), _n('<span>%</span> Response', '<span>%</span> Responses', get_comments_number(),'jointstheme') );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
-
+	<h3 id="comments" class="h2"><?php printf( _n( '<span>No</span> Responses to &ldquo;%2$s&rdquo;', 'One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'soblossom' ), number_format_i18n( get_comments_number() ), '&#8220;' . get_the_title() . '&#8221;' ); ?></h3>
 	
 	<ol class="commentlist">
 		<?php wp_list_comments('type=comment&callback=joints_comments'); ?>
@@ -35,7 +34,7 @@ The comments page for joints
 	<?php else : // comments are closed ?>
 	
 	<!-- If comments are closed. -->
-	<!--p class="nocomments"><?php _e("Comments are closed.", "jointstheme"); ?></p-->
+	<!--p class="nocomments"><?php _e( 'Comments are closed.', 'jointswp' ); ?></p-->
 
 	<?php endif; ?>
 
@@ -46,7 +45,7 @@ The comments page for joints
 
 <section id="respond" class="respond-form">
 
-	<h3 id="comment-form-title" class="h2"><?php comment_form_title( __('Leave a Reply', 'jointstheme'), __('Leave a Reply to %s', 'jointstheme' )); ?></h3>
+	<h3 id="comment-form-title" class="h2"><?php comment_form_title( __( 'Leave a Reply', 'jointswp' ), __( 'Leave a Reply to %s', 'jointswp'  )); ?></h3>
 
 	<div id="cancel-comment-reply">
 		<p class="small"><?php cancel_comment_reply_link(); ?></p>
@@ -54,7 +53,7 @@ The comments page for joints
 
 	<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
   	<div class="alert help">
-  		<p><?php printf( __('You must be %1$slogged in%2$s to post a comment.', 'jointstheme'), '<a href="<?php echo wp_login_url( get_permalink() ); ?>">', '</a>' ); ?></p>
+  		<p><?php printf( __( 'You must be %1$slogged in%2$s to post a comment.', 'jointswp' ), '<a href="<?php echo wp_login_url( get_permalink() ); ?>">', '</a>' ); ?></p>
   	</div>
 	<?php else : ?>
 
@@ -62,36 +61,36 @@ The comments page for joints
 
 	<?php if ( is_user_logged_in() ) : ?>
 
-	<p class="comments-logged-in-as"><?php _e("Logged in as", "jointstheme"); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e("Log out of this account", "jointstheme"); ?>"><?php _e("Log out", "jointstheme"); ?> <?php _e("&raquo;", "jointstheme"); ?></a></p>
+	<p class="comments-logged-in-as"><?php _e( 'Logged in as', 'jointswp' ); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e( 'Log out of this account', 'jointswp' ); ?>"><?php _e( 'Log out', 'jointswp' ); ?> <?php _e( '&raquo;', 'jointswp' ); ?></a></p>
 
 	<?php else : ?>
 	
 	<ul id="comment-form-elements" class="clearfix">
 		
 		<li>
-		  <label for="author"><?php _e("Name", "jointstheme"); ?> <?php if ($req) _e("(required)"); ?></label>
-		  <input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" placeholder="<?php _e('Your Name*', 'jointstheme'); ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+		  <label for="author"><?php _e( 'Name', 'jointswp' ); ?> <?php if ($req) _e( '(required)', 'jointswp' ); ?></label>
+		  <input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" placeholder="<?php _e( 'Your Name*', 'jointswp' ); ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
 		</li>
 		
 		<li>
-		  <label for="email"><?php _e("Mail", "jointstheme"); ?> <?php if ($req) _e("(required)"); ?></label>
-		  <input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" placeholder="<?php _e('Your E-Mail*', 'jointstheme'); ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-		  <small><?php _e("(will not be published)", "jointstheme"); ?></small>
+		  <label for="email"><?php _e( 'Mail', 'jointswp' ); ?> <?php if ($req) _e( '(required)', 'jointswp' ); ?></label>
+		  <input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" placeholder="<?php _e( 'Your E-Mail*', 'jointswp' ); ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+		  <small><?php _e( '(will not be published)', 'jointswp' ); ?></small>
 		</li>
 		
 		<li>
-		  <label for="url"><?php _e("Website", "jointstheme"); ?></label>
-		  <input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" placeholder="<?php _e('Got a website?', 'jointstheme'); ?>" tabindex="3" />
+		  <label for="url"><?php _e( 'Website', 'jointswp' ); ?></label>
+		  <input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" placeholder="<?php _e( 'Got a website?', 'jointswp' ); ?>" tabindex="3" />
 		</li>
 		
 	</ul>
 
 	<?php endif; ?>
 	
-	<p><textarea name="comment" id="comment" placeholder="<?php _e('Your Comment here...', 'jointstheme'); ?>" tabindex="4"></textarea></p>
+	<p><textarea name="comment" id="comment" placeholder="<?php _e( 'Your Comment here...', 'jointswp' ); ?>" tabindex="4"></textarea></p>
 	
 	<p>
-	  <input name="submit" type="submit" id="submit" class="button" tabindex="5" value="<?php _e('Submit', 'jointstheme'); ?>" />
+	  <input name="submit" type="submit" id="submit" class="button" tabindex="5" value="<?php _e( 'Submit', 'jointswp' ); ?>" />
 	  <?php comment_id_fields(); ?>
 	</p>
 	
