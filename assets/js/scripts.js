1fcd0666f5fdf289e516f90b3fc9eaa14dfdace7
Piet Bos <info@senlinonline.com>
2015-11-25 16:34:58 +0800

    add class of end to archive grid
    this to not have adverse effect that last grid item is floating right

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,5 +6,14 @@ and Foundation play nice together.
 
 // Remove empty P tags created by WP inside of Accordion and Orbit
 jQuery(document).ready(function() {
+
     jQuery('.accordion p:empty, .orbit p:empty').remove();
+
+	/**
+	 * For parts/loop-archive-grid.php
+	 * When the total number of posts is not dividable by 3, the last post float to the right.
+	 * This can be fixed by adding the end class to the last item, which then floats the last item to the left instead.
+	 */
+	jQuery( '.category-grid .columns.panel' ).last().addClass( 'end' );
+
 });
