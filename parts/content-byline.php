0b236bd27b31439cd952301714e1472f7424b13d
Piet Bos <info@senlinonline.com>
2015-11-25 14:15:06 +0800

    commit corrected and tested byline part

--- parts/content-byline.php
+++ parts/content-byline.php
@@ -1,12 +1,8 @@
-<p class="byline">
-	<?php
-		
-		/* Translators: %1$s is the time, %2$s is the link to the author's archive, %3$s is the category */
-		printf( __( 'Posted on %1$s by %2$s - %3$s', 'jointswp' ),
-			the_time( 'F j, Y' ),
-			the_author_posts_link(),
-			the_category( ', ' )
-		);
-		
-	?>
-</p>	
+<?php
+	/* Translators: %1$s is the time, %2$s is the link to the author's archive, %3$s is the category */
+	printf( '<p class="byline">' . __( 'Posted on %1$s by %2$s - %3$s', 'jointswp' ) . '</p>',
+		get_the_time( 'F j, Y' ),
+		get_the_author_posts_link(),
+		get_the_category_list( __( ', ', 'jointswp' ) )
+	);
+?>
