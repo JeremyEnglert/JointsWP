e05ecbc99bdc6ff3b62596921480365501c2d1de
Jeremy Englert <jeremy@cmsadgroup.com>
2015-11-28 12:19:01 -0700

    Call Unmagnified CSS
    Because not everyone uses Gulp/Grunt

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -18,7 +18,7 @@ function site_scripts() {
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', true );
    
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.min.css', array(), '', 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
