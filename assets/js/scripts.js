bd4d039d4f51048ee28a1855fa08eca6d2de553e
Jeremy Englert <jeremy@cmsadgroup.com>
2015-12-01 20:43:48 -0700

    Automatically add flex-video to video embeds

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,15 +4,15 @@ These functions make sure WordPress
 and Foundation play nice together.
 */
 
-// Remove empty P tags created by WP inside of Accordion and Orbit
 jQuery(document).ready(function() {
     
+    // Remove empty P tags created by WP inside of Accordion and Orbit
     jQuery('.accordion p:empty, .orbit p:empty').remove();
     
-    /**
-	 * For parts/loop-archive-grid.php
-	 * Makes sure last grid item floats left
-	 */
+	 // Makes sure last grid item floats left
 	jQuery( '.archive-grid .columns' ).last().addClass( 'end' );
+	
+	// Adds Flex Video to YouTube and Vimeo Embeds
+	jQuery( 'iframe[src*="youtube.com"], iframe[src*="vimeo.com"]' ).wrap("<div class='flex-video'/>");
 
 });
