1c19705c54c8f4f3a4825ecf203abae073ad077e
Jeremy Englert <jeremy@cmsadgroup.com>
2016-03-01 20:47:53 -0700

    Combined menu functions into one file

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -18,7 +18,15 @@ function joints_top_nav() {
         'fallback_cb' => false,                         // Fallback function (see below)
         'walker' => new Topbar_Menu_Walker()
     ));
-} /* End Top Menu */
+} 
+
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+class Topbar_Menu_Walker extends Walker_Nav_Menu {
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"menu\">\n";
+    }
+}
 
 // The Off Canvas Menu
 function joints_off_canvas_nav() {
@@ -31,7 +39,14 @@ function joints_off_canvas_nav() {
         'fallback_cb' => false,                         // Fallback function (see below)
         'walker' => new Off_Canvas_Menu_Walker()
     ));
-} /* End Off Canvas Menu */
+} 
+
+class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"menu\">\n";
+    }
+}
 
 // The Footer Menu
 function joints_footer_links() {
--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
 
 // Register custom menus and menu walkers
 require_once(get_template_directory().'/assets/functions/menu.php'); 
-require_once(get_template_directory().'/assets/functions/menu-walkers.php'); 
 
 // Register sidebars/widget areas
 require_once(get_template_directory().'/assets/functions/sidebar.php'); 
