c1487f80fcb4c710754fc5176c81fe2808c16c70
Jeremy Englert <jeremy@theadgarage.com>
2016-03-11 15:02:29 -0700

    Fixed accordion menu

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -44,7 +44,7 @@ function joints_off_canvas_nav() {
 class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
     function start_lvl(&$output, $depth = 0, $args = Array() ) {
         $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"menu\">\n";
+        $output .= "\n$indent<ul class=\"vertical menu\">\n";
     }
 }
 
