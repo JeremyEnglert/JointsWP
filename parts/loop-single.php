444aa98326307a54937f42b5820efac7eb9f88fa
Jeremy Englert <jeremy@theadgarage.com>
2016-03-15 16:13:57 -0700

    Added wp_link_pages support

--- parts/loop-single.php
+++ parts/loop-single.php
@@ -11,8 +11,10 @@
 	</section> <!-- end article section -->
 						
 	<footer class="article-footer">
-		<p class="tags"><?php the_tags('<span class="tags-title">' . __( 'Tags:', 'jointswp' ) . '</span> ', ', ', ''); ?></p>	</footer> <!-- end article footer -->
-									
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jointswp' ), 'after'  => '</div>' ) ); ?>
+		<p class="tags"><?php the_tags('<span class="tags-title">' . __( 'Tags:', 'jointswp' ) . '</span> ', ', ', ''); ?></p>	
+	</footer> <!-- end article footer -->
+						
 	<?php comments_template(); ?>	
 													
 </article> <!-- end article -->
