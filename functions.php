3b703249a129ee5a994ba23b06ba0115b7979df5
Jeremy Englert <jeremy@theadgarage.com>
2016-03-25 15:29:30 -0700

    Moved function files to /includes directory

--- functions.php
+++ functions.php
@@ -1,42 +1,42 @@
 <?php
 // Theme support options
-require_once(get_template_directory().'/assets/functions/theme-support.php'); 
+require_once(get_template_directory().'/includes/theme-support.php'); 
 
 // WP Head and other cleanup functions
-require_once(get_template_directory().'/assets/functions/cleanup.php'); 
+require_once(get_template_directory().'/includes/cleanup.php'); 
 
 // Register scripts and stylesheets
-require_once(get_template_directory().'/assets/functions/enqueue-scripts.php'); 
+require_once(get_template_directory().'/includes/enqueue-scripts.php'); 
 
 // Register custom menus and menu walkers
-require_once(get_template_directory().'/assets/functions/menu.php'); 
+require_once(get_template_directory().'/includes/menu.php'); 
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/assets/functions/sidebar.php'); 
+require_once(get_template_directory().'/includes/sidebar.php'); 
 
 // Makes WordPress comments suck less
-require_once(get_template_directory().'/assets/functions/comments.php'); 
+require_once(get_template_directory().'/includes/comments.php'); 
 
 // Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/assets/functions/page-navi.php'); 
+require_once(get_template_directory().'/includes/page-navi.php'); 
 
 // Adds support for multiple languages
-require_once(get_template_directory().'/assets/translation/translation.php'); 
+require_once(get_template_directory().'/includes/translation/translation.php'); 
 
 // Adds site styles to the WordPress editor
-// require_once(get_template_directory().'/assets/functions/editor-styles.php'); 
+// require_once(get_template_directory().'/includes/editor-styles.php'); 
 
 // Remove 4.2 Emoji Support
-// require_once(get_template_directory().'/assets/functions/disable-emoji.php'); 
+// require_once(get_template_directory().'/includes/disable-emoji.php'); 
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/assets/functions/related-posts.php'); 
+// require_once(get_template_directory().'/includes/related-posts.php'); 
 
 // Use this as a template for custom post types
-// require_once(get_template_directory().'/assets/functions/custom-post-type.php');
+// require_once(get_template_directory().'/includes/custom-post-type.php');
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/assets/functions/login.php'); 
+// require_once(get_template_directory().'/includes/login.php'); 
 
 // Customize the WordPress admin
-// require_once(get_template_directory().'/assets/functions/admin.php'); 
+// require_once(get_template_directory().'/includes/admin.php'); 
