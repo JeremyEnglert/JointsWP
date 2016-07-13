2ff21682635ee5d97b94ce2ececde29c0ea40072
Jędrzej Skrzypczak <Yurciu@users.noreply.github.com>
2016-07-13 15:31:18 +0200

    Fix for mobile off-canvas menu
    Prevent reveal by slide inactive but visible off-canvas menu.

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -1,3 +1,3 @@
-<div class="off-canvas position-right" id="off-canvas" data-off-canvas data-position="right">
+<div class="off-canvas position-right show-for-small-only" id="off-canvas" data-off-canvas data-position="right">
 	<?php joints_off_canvas_nav(); ?>
 </div>
