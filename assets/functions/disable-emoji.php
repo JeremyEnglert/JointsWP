e6164b631cde05fc64cd680cd55bec3567a24b94
Piet Bos <piet@so-wp.com>
2016-12-19 14:29:36 -0100

    filter that removes DNS prefetch s.w.org (used for emojis, since WP 4.7)

--- assets/functions/disable-emoji.php
+++ assets/functions/disable-emoji.php
@@ -13,6 +13,9 @@ function disable_wp_emoji() {
 
   // filter to remove TinyMCE emojis
   add_filter( 'tiny_mce_plugins', 'disable_emoji_tinymce' );
+  
+  // filter that removes DNS prefetch s.w.org (used for emojis, since WP 4.7)
+  add_filter( 'emoji_svg_url', '__return_false' );
 }
 add_action( 'init', 'disable_wp_emoji' );
 
