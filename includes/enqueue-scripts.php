bcd74de8d0e745441982412db679f4a88c8cd059
Jeremy Englert <jeremy@cmsadgroup.com>
2017-02-06 23:37:35 -0700

    Added version number for easier cache busting

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -3,7 +3,7 @@ function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
         
     // Adding scripts file in the footer
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', true );
+    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), "5.0", true );
    
     // Register main stylesheet
     wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), time(), 'all' );
