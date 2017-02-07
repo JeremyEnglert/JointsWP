141b991c690a53781d6beef99faa2347413e58a3
Jeremy Englert <jeremy@cmsadgroup.com>
2017-02-07 01:20:52 -0700

    Improved page-navi function

--- includes/page-navi.php
+++ includes/page-navi.php
@@ -1,56 +1,29 @@
-<?php
-// Numeric Page Navi (built into the theme by default)
-function joints_page_navi($before = '', $after = '') {
-	global $wpdb, $wp_query;
-	$request = $wp_query->request;
-	$posts_per_page = intval(get_query_var('posts_per_page'));
-	$paged = intval(get_query_var('paged'));
-	$numposts = $wp_query->found_posts;
-	$max_page = $wp_query->max_num_pages;
-	if ( $numposts <= $posts_per_page ) { return; }
-	if(empty($paged) || $paged == 0) {
-		$paged = 1;
-	}
-	$pages_to_show = 7;
-	$pages_to_show_minus_1 = $pages_to_show-1;
-	$half_page_start = floor($pages_to_show_minus_1/2);
-	$half_page_end = ceil($pages_to_show_minus_1/2);
-	$start_page = $paged - $half_page_start;
-	if($start_page <= 0) {
-		$start_page = 1;
-	}
-	$end_page = $paged + $half_page_end;
-	if(($end_page - $start_page) != $pages_to_show_minus_1) {
-		$end_page = $start_page + $pages_to_show_minus_1;
-	}
-	if($end_page > $max_page) {
-		$start_page = $max_page - $pages_to_show_minus_1;
-		$end_page = $max_page;
-	}
-	if($start_page <= 0) {
-		$start_page = 1;
-	}
-	echo $before.'<nav class="page-navigation"><ul class="pagination">'."";
-	if ($start_page >= 2 && $pages_to_show < $max_page) {
-		$first_page_text = __( 'First', 'jointswp' );
-		echo '<li><a href="'.get_pagenum_link().'" title="'.$first_page_text.'">'.$first_page_text.'</a></li>';
-	}
-	echo '<li>';
-	previous_posts_link( __('Previous', 'jointswp') );
-	echo '</li>';
-	for($i = $start_page; $i  <= $end_page; $i++) {
-		if($i == $paged) {
-			echo '<li class="current"> '.$i.' </li>';
-		} else {
-			echo '<li><a href="'.get_pagenum_link($i).'">'.$i.'</a></li>';
+<?php // Borrowed with love from FoundationPress
+	function joints_page_navi() {
+		global $wp_query;
+		$big = 999999999; // This needs to be an unlikely integer
+		// For more options and info view the docs for paginate_links()
+		// http://codex.wordpress.org/Function_Reference/paginate_links
+		$paginate_links = paginate_links( array(
+			'base' => str_replace( $big, '%#%', html_entity_decode( get_pagenum_link( $big ) ) ),
+			'current' => max( 1, get_query_var( 'paged' ) ),
+			'total' => $wp_query->max_num_pages,
+			'mid_size' => 5,
+			'prev_next' => true,
+		    'prev_text' => __( '&laquo;', 'jointswp' ),
+		    'next_text' => __( '&raquo;', 'jointswp' ),
+			'type' => 'list',
+		) );
+		$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination'>", $paginate_links );
+		$paginate_links = str_replace( '<li><span class="page-numbers dots">', "<li><a href='#'>", $paginate_links );
+		$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'>", $paginate_links );
+		$paginate_links = str_replace( '</span>', '</a>', $paginate_links );
+		$paginate_links = str_replace( "<li><a href='#'>&hellip;</a></li>", "<li><span class='dots'>&hellip;</span></li>", $paginate_links );
+		$paginate_links = preg_replace( '/\s*page-numbers/', '', $paginate_links );
+		// Display the pagination if more than one page is found.
+		if ( $paginate_links ) {
+			echo '<div class="page-navigation">';
+			echo $paginate_links;
+			echo '</div><!--// end .pagination -->';
 		}
 	}
-	echo '<li>';
-	next_posts_link( __('Next', 'jointswp'), 0 );
-	echo '</li>';
-	if ($end_page < $max_page) {
-		$last_page_text = __( 'Last', 'jointswp' );
-		echo '<li><a href="'.get_pagenum_link($max_page).'" title="'.$last_page_text.'">'.$last_page_text.'</a></li>';
-	}
-	echo '</ul></nav>'.$after."";
-} /* End page navi */
