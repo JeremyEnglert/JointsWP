7109ce2ee4fb2d1f9881f1ed76188bff38a45f00
Robert Jakobson <robert@veebidisainer.ee>
2017-02-15 15:47:51 +0200

    Adding small-12 classes for the main content areas and the sidebar
    While using a flex-grid I noticed that the main content area and the
    sidebar do not stack below each other for smaller screens.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
+<div id="sidebar1" class="sidebar small-12 large-4 medium-4 columns" role="complementary">
 
 	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
