45e82cea0ff71a24cbfea112aa41b9f7fb8cdc58
Jeremy Englert <jeremy@cmsadgroup.com>
2017-02-21 22:48:19 -0700

    Cache busting

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -3,10 +3,10 @@ function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
         
     // Adding scripts file in the footer
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), "5.0", true );
+    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), filemtime(get_template_directory() . '/assets/js/scripts.js'), true );
    
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), time(), 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_template_directory() . '/assets/css/style.css'), 'all' );
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
