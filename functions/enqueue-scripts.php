f2d68bdd053a6426db1fcec2b847fef94de4a7dc
Jeremy Englert <jeremy@cmsadgroup.com>
2017-03-15 21:07:34 -0700

    Moved files to assets directory

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -3,10 +3,10 @@ function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
         
     // Adding scripts file in the footer
-    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/scripts/scripts.js', array( 'jquery' ), filemtime(get_template_directory() . '/scripts/js'), true );
+    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js', array( 'jquery' ), filemtime(get_template_directory() . '/assets/scripts/js'), true );
    
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/styles/style.css', array(), filemtime(get_template_directory() . '/styles/scss'), 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array(), filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
