176eb799a51b13976f878bf1ef22c64f6800a0a9
Jeremy Englert <jeremy@cmsadgroup.com>
2017-06-03 00:49:07 -0700

    Removed unnecessary meta

--- header.php
+++ header.php
@@ -25,13 +25,7 @@
 		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
 			<!-- Icons & Favicons -->
 			<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-			<link href="<?php echo get_template_directory_uri(); ?>/images/apple-icon-touch.png" rel="apple-touch-icon" />
-			<!--[if IE]>
-				<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-			<![endif]-->
-			<meta name="msapplication-TileColor" content="#f01d4f">
-			<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/win8-tile-icon.png">
-	    	<meta name="theme-color" content="#121212">
+			<link href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />	
 	    <?php } ?>
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
