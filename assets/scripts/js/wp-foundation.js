d6d164c9d3bcc06401d078ef1c79db39b79c1534
Jeremy Englert <jeremy@cmsadgroup.com>
2017-07-04 18:49:14 -0700

    Removed un-used script from older JWP

--- assets/scripts/js/wp-foundation.js
+++ assets/scripts/js/wp-foundation.js
@@ -8,15 +8,12 @@ jQuery(document).ready(function() {
     // Remove empty P tags created by WP inside of Accordion and Orbit
     jQuery('.accordion p:empty, .orbit p:empty').remove();
 
-	 // Makes sure last grid item floats left
-	jQuery('.archive-grid .columns').last().addClass( 'end' );
-
 	// Adds Flex Video to YouTube and Vimeo Embeds
 	jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
 		if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
-		  jQuery(this).wrap("<div class='widescreen flex-video'/>");
+		  jQuery(this).wrap("<div class='widescreen responsive-embed'/>");
 		} else {
-		  jQuery(this).wrap("<div class='flex-video'/>");
+		  jQuery(this).wrap("<div class='responsive-embed'/>");
 		}
 	});
 });
