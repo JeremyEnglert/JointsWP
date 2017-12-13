877abb41d08f5f01458c76e5e35a716255b5ce5b
Garrett Hyder <garrett@eclipsecreative.ca>
2017-12-13 09:51:58 -0800

    Add offcanvas sidebar to content-offcanvas
    This is to resolve #299 properly.
    @JeremyEnglert let me know what you think, should this just be commented out?

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -8,4 +8,19 @@
 
 <div class="off-canvas position-right" id="off-canvas" data-off-canvas>
 	<?php joints_off_canvas_nav(); ?>
+
+	<?php if ( is_active_sidebar( 'offcanvas' ) ) : ?>
+
+		<?php dynamic_sidebar( 'offcanvas' ); ?>
+
+	<?php else : ?>
+
+	<!-- This content shows up if there are no widgets defined in the backend. -->
+						
+	<div class="alert help">
+		<p><?php _e( 'Please activate some Widgets.', 'jointswp' );  ?></p>
+	</div>
+
+	<?php endif; ?>
+
 </div>
