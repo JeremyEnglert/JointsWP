9c430aca52d3e5b43dbedfb1e566e5967e83cd0c
Garrett Hyder <garrett@eclipsecreative.ca>
2018-01-07 21:53:48 -0800

    Migrate sidebar add_action into sidebar.php for easier removal, corrects #307 and fixed PR #308

--- functions/cleanup.php
+++ functions/cleanup.php
@@ -7,19 +7,16 @@ function joints_start() {
 
     // launching operation cleanup
     add_action('init', 'joints_head_cleanup');
-    
+
     // remove pesky injected css for recent comments widget
     add_filter( 'wp_head', 'joints_remove_wp_widget_recent_comments_style', 1 );
-    
+
     // clean up comment styles in the head
     add_action('wp_head', 'joints_remove_recent_comments_style', 1);
-    
+
     // clean up gallery output in wp
     add_filter('gallery_style', 'joints_gallery_style');
-    
-    // adding sidebars to Wordpress
-    add_action( 'widgets_init', 'joints_register_sidebars' );
-    
+
     // cleaning up excerpt
     add_filter('excerpt_more', 'joints_excerpt_more');
 
@@ -80,7 +77,7 @@ function remove_sticky_class($classes) {
 		$classes = array_diff($classes, array("sticky"));
 		$classes[] = 'wp-sticky';
 	}
-	
+
 	return $classes;
 }
 add_filter('post_class','remove_sticky_class');
