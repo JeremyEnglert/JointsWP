0604af24ea0091cbb2337460e74b394ba568a99a
Garrett Hyder <garrett@eclipse3sixty.com>
2018-01-11 21:41:59 -0800

    Separating main-nav from offcanvas-nav
    Made theme_locations unique to avoid ID's being stripped from menu id's.
    Fixes #225
    *Note I also added menu_id to the wp_nav_menu so you can more easily detect them within Walkers.

--- functions/menu.php
+++ functions/menu.php
@@ -3,6 +3,7 @@
 register_nav_menus(
 	array(
 		'main-nav' => __( 'The Main Menu', 'jointswp' ),   // Main nav in header
+		'offcanvas-nav' => __( 'The Off-Canvas Menu', 'jointswp' ),   // Off-Canvas nav
 		'footer-links' => __( 'Footer Links', 'jointswp' ) // Secondary nav in footer
 	)
 );
@@ -11,6 +12,7 @@ register_nav_menus(
 function joints_top_nav() {
 	 wp_nav_menu(array(
         'container' => false,                           // Remove nav container
+	'menu_id' => 'main-nav',       			// Adding custom nav id
         'menu_class' => 'medium-horizontal menu',       // Adding custom nav class
         'items_wrap' => '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
         'theme_location' => 'main-nav',        			// Where it's located in the theme
@@ -32,9 +34,10 @@ class Topbar_Menu_Walker extends Walker_Nav_Menu {
 function joints_off_canvas_nav() {
 	 wp_nav_menu(array(
         'container' => false,                           // Remove nav container
+	'menu_id' => 'offcanvas-nav',       			// Adding custom nav id
         'menu_class' => 'vertical menu accordion-menu',       			// Adding custom nav class
         'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
-        'theme_location' => 'main-nav',        			// Where it's located in the theme
+        'theme_location' => 'offcanvas-nav',        			// Where it's located in the theme
         'depth' => 5,                                   // Limit the depth of the nav
         'fallback_cb' => false,                         // Fallback function (see below)
         'walker' => new Off_Canvas_Menu_Walker()
@@ -53,6 +56,7 @@ function joints_footer_links() {
     wp_nav_menu(array(
     	'container' => 'false',                         // Remove nav container
     	'menu' => __( 'Footer Links', 'jointswp' ),   	// Nav name
+	'menu_id' => 'footer-links',       			// Adding custom nav id
     	'menu_class' => 'menu',      					// Adding custom nav class
     	'theme_location' => 'footer-links',             // Where it's located in the theme
         'depth' => 0,                                   // Limit the depth of the nav
