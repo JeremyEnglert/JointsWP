1e52ca20f06059841a82256775260c96880b8b57
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2018-10-10 21:59:01 -0700

    Add ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "rules": {
+		// View possible rules: https://eslint.org/docs/rules/
+	},
+	"globals": {
+		'document': true,
+		'jQuery': true,
+		'$': true
+	},
+	"env": {
+		"browser": true,
+		"node": true,
+		"es6": true
+	  },
+}
