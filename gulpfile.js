3e57af15579bc14cc1b302f8606462f40636d68a
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2018-10-14 15:21:36 -0700

    Start p11ly integration

--- gulpfile.js
+++ gulpfile.js
@@ -29,17 +29,24 @@ const eslint = require('gulp-eslint'); // Checks JS files for errors
 const concat = require('gulp-concat'); // Combine JS files
 const uglify = require('gulp-uglify'); // Minify JS files
 
+// Image Plugins
+const imagemin = require('gulp-imagemin'); // Comopress images
+
+// Translation Plugins
+const wpPot = require('gulp-wp-pot');
+
+// Accessibility Plugins
+const pa11y = require('pa11y');
+
 // Utility Plugins
 const fs = require('fs'); // Allows access to file system
 const sourcemaps = require( 'gulp-sourcemaps' ); // Generates sourcemaps for minified files
 const plumber = require( 'gulp-plumber' ); // Prevent pipe breaking caused by errors from gulp plugins
 const browserSync = require('browser-sync').create(); // Reloads browser and injects CSS
-const cached = require('gulp-cached'); // Keeps an in-memory cache of files
-const remember = require('gulp-remember'); // Remembers files that have passed through it
 const chalk = require('chalk'); // Add color to console logs
 const beeper = require('beeper'); // Attention grabber when an error happens
 
-const wpPot = require('gulp-wp-pot');
+
 
 /**
  * Gulp Functions.
@@ -58,6 +65,7 @@ const reload = done => {
 	done();
 }
 
+
 // Browser-Sync watch files and inject changes
 gulp.task('watch', () => {
 	
@@ -73,7 +81,7 @@ gulp.task('watch', () => {
     gulp.watch(config.source.styles, gulp.parallel('styles'));
     gulp.watch(config.source.scripts, gulp.series('scripts', reload));
     gulp.watch(config.source.images, gulp.series('images', reload));
-
+    gulp.watch(config.source.php, gulp.series(reload));
 });
 
 // Compile Sass, Autoprefix and minify
@@ -112,12 +120,13 @@ gulp.task('scripts', () => {
 
 
 // Optimize images, move into assets directory
-gulp.task('images', function() {
-	return gulp.src(source.images)
+gulp.task('images', () => {
+	return gulp.src(config.source.images)
 		.pipe(imagemin())
-		.pipe(gulp.dest(assets.images))
+		.pipe(gulp.dest(config.assets.images))
 });
 
+// Create .PO translation files
  gulp.task( 'translate', function () {
      return gulp.src( config.source.php )
          .pipe(wpPot( {
@@ -127,5 +136,45 @@ gulp.task('images', function() {
         .pipe(gulp.dest('file.pot'));
  });
 
-// Run styles, scripts and foundation-js
-gulp.task('default', gulp.parallel('styles', 'scripts', 'watch'));
+ gulp.task( "pa11y", async () => {
+	try {
+
+		let pa11yTestingUrls = [];
+		
+		for(let url in config.accessibilityTestingUrls) {
+			const formattedUrl =  pa11y(`${config.accessibilityTestingUrls[url]}`);
+			pa11yTestingUrls.push(formattedUrl);
+		};
+		
+		// Run tests against multiple URLs
+		const results = await Promise.all(
+			pa11yTestingUrls
+		);
+
+		// Format the errors
+		const printResult = (result) => {
+			result.issues.forEach(function(issue) {
+				console.log(chalk.red.bold(`${result.pageUrl}`));
+				console.log(`Issue: ${issue.message}`);
+				console.log(`Code: ${issue.code}`);
+				console.log(`Context: ${issue.context}`);
+				console.log(`Selector: ${issue.selector}`);
+				console.log("--------------------------------");
+			});
+		};
+
+		// Output the raw result objects
+		results.forEach(function(result) {
+			printResult(result); // Results for the first URL
+		});
+
+	} catch (error) {
+
+		// Output an error if it occurred
+		console.error(error.message);
+
+	}
+ });
+
+// Run styles, scripts and images - then start Browsersync
+gulp.task('default', gulp.series(gulp.parallel('styles', 'scripts', 'images'), 'watch'));
