271b5df5618e51c85d234ebe5cc6bc276fb43c4e
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2018-11-11 14:55:42 -0700

    Removed unused meta

--- header.php
+++ header.php
@@ -12,22 +12,12 @@
   <html class="no-js"  <?php language_attributes(); ?>>
 
 	<head>
+	
 		<meta charset="utf-8">
-		
-		<!-- Force IE to use the latest rendering engine available -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 		<!-- Mobile Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta class="foundation-mq">
 		
-		<!-- If Site Icon isn't set in customizer -->
-		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
-			<!-- Icons & Favicons -->
-			<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-			<link href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />	
-	    <?php } ?>
-
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<?php wp_head(); ?>
@@ -38,10 +28,12 @@
 				
 		<div class="site-wrapper">
 			
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'jointswp' ); ?></a>
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'theme_namespace' ); ?></a>
 
 			<header class="header" role="banner">
 						
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-
+				
 			</header> <!-- end .header -->
+
+			<?php theme_namespace_top_nav(); ?>
