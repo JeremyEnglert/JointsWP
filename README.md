4419316bb5939590788327b6b0c8d041dbbec9f4
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2018-12-05 00:13:58 -0700

    Update to Foundation 6.5.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Currently using Foundation 6.4.3.
+Currently using Foundation 6.5.1.
 
 ## JointsWP Requirements
 JointsWP requires [Node.js](https://nodejs.org) v6.9.x or newer. This doesn't mean you need to understand Node (or even Gulp) - it's just the steps we need to take to make sure all of our development tools are installed. 
--- assets/scripts/js/wp-foundation.js
+++ assets/scripts/js/wp-foundation.js
@@ -5,8 +5,8 @@ and Foundation play nice together.
 
 jQuery(document).ready(function() {
 	
-    // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
+	// Remove empty P tags created by WP inside of Accordion and Orbit
+	jQuery('.accordion p:empty, .orbit p:empty').remove();
 
 	// Adds Flex Video to YouTube and Vimeo Embeds
 	jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
