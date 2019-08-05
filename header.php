5301dcbeac07059e1adb28bc59b6d1895a43032e
Jeremy Englert <hi@jem.codes>
2019-08-04 23:31:46 -0700

    Improve comments

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
  * The template for displaying the header
  *
  * This is the template that displays all of the <head> section
- *
  */
 ?>
 
@@ -14,10 +13,7 @@
 	<head>
 	
 		<meta charset="utf-8">
-
-		<!-- Mobile Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">		
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<?php wp_head(); ?>
@@ -27,16 +23,20 @@
 	<body <?php body_class(); ?>>
 				
 		<div class="site-wrapper">
-			
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'theme_namespace' ); ?></a>
+			<!--
+			https://make.wordpress.org/accessibility/handbook/markup/skip-links/
+			It's okay to have multiple skip links.
+			For example, an additional skip link to jump to a footer menu.
+			-->
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to Content', 'theme_namespace' ); ?></a>
 
 			<header class="header" role="banner">
 						
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				
-			</header> <!-- end .header -->
+			</header> 
 
-			<nav class="main-navigation-wrapper" role="navigation">
+			<nav class="main-navigation-wrapper" aria-label="Main">
 
 				<?php theme_namespace_top_nav(); ?>
 				
