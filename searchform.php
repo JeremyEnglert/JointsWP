e54f480bda5d10b4851a299a695cf6430ea2d5b3
Jeremy Englert <hi@jem.codes>
2019-08-09 00:54:09 -0700

    Better search form

--- searchform.php
+++ searchform.php
@@ -1,13 +1,19 @@
 <?php
 /**
  * The template for displaying search form
+ * 
+ * Usage: get_search_form()
+ * https://developer.wordpress.org/reference/functions/get_search_form/
  */
  ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'theme_namespace' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search...', 'theme_namespace' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'theme_namespace' ) ?>" />
-	</label>
-	<input type="submit" class="search-submit button" value="<?php echo esc_attr_x( 'Search', 'theme_namespace' ) ?>" />
-</form>
+<div itemscope itemtype="http://schema.org/WebSite">
+	<form role="search" id="searchform" class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<meta itemprop="target" content="<?php echo esc_url( home_url() ); ?>/?s={s}" />
+		<label for="search-field">
+			<?php echo esc_html_x( 'Search for:', 'label', 'theme_namespace' ); ?>
+		</label>
+		<input itemprop="query-input" type="search" id="search-field" value="<?php echo get_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'tenup' ); ?>" name="s" />
+		<input type="submit" value="<?php echo esc_attr_x( 'Search', 'theme_namespace' ); ?>">
+	</form>
+</div>
