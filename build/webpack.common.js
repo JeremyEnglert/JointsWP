973348c7f13ea493ba23ab0b309107baecf2cc06
Jeremy Englert <hi@jem.codes>
2019-08-09 00:33:58 -0700

    Improve webpack babel integration

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -44,7 +44,6 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
             sourceMap: true,
          }
         }
